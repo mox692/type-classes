22c635c9d662d6013a27a85e24867b5fe506871d
mox692 <55653825+mox692@users.noreply.github.com>
2022-10-23 18:19:16 +0900

    add types

--- Base.hs
+++ Base.hs
@@ -1,4 +1,4 @@
--- haskellの型クラスの再定義的な
+-- ref: https://hackage.haskell.org/package/base-4.17.0.0/docs/src/GHC.Base.html
 
 --
 -- Functor
@@ -10,23 +10,43 @@ instance Functor' [] where
     fmap' f [] = []
     fmap' f (h:t) = f h : fmap f t
 
+instance Functor' Maybe  where
+    fmap' f Nothing = Nothing
+    fmap' f (Just x) = Just $ f x
+
+
 --
--- Functor
+-- Applicative
 --
+class Applicative' f where
+    pure' :: a -> f a
+    apply' :: f (a -> b) -> f a -> f b -- In GHC.Base, it is called `<*>`.
+
+instance Applicative' [] where
+    pure' a = [a]
+    apply' fs [] = []
+    apply' [] xs = []
+    apply' fs xs = [f x | f <- fs, x <- xs]
+
+instance Applicative' Maybe where
+    pure' a = Just a
+    apply' f Nothing = Nothing
+    apply' Nothing (Just x) = Nothing
+    apply' (Just f) (Just x) = Just $ f x
 
 --
 -- Semigroup
 --
 class Semigroup' a where
-    merge :: a -> a -> a
+    merge' :: a -> a -> a -- In GHC.Base, it is called `<>`.
 
 instance Semigroup' [a] where
-    merge x y = x ++ y
+    merge' x y = x ++ y
 
 instance Semigroup' a => Semigroup' (Maybe a) where
-    merge Nothing x = x
-    merge x Nothing = x
-    merge (Just x) (Just y) = Just $ x `merge` y
+    merge' Nothing x = x
+    merge' x Nothing = x
+    merge' (Just x) (Just y) = Just $ x `merge'` y
 
 --
 -- Monoid
@@ -37,24 +57,38 @@ class Semigroup' a =>  Monoid' a where
 
 instance  Monoid' [a] where
     mempty' = []
-    mappend' = merge
+    mappend' = merge'
+
+instance Semigroup' a => Monoid' (Maybe a) where
+    mempty' = Nothing
+    mappend' = merge'
 
 --
 -- Foldable
---
+-- 
 class Foldable' t where
+    -- その型の中身を取り出して、monoidに写すような関数が適用できる型であるか
     foldMap' :: Monoid' m => (a -> m) -> t a -> m
 
 instance Foldable' [] where
     foldMap' f [] = mempty'
     foldMap' f (h:t) = f h `mappend'` foldMap' f t
 
+instance Foldable' Maybe where
+    foldMap' f Nothing = mempty'
+    foldMap' f (Just x) = f x
+
 --
 -- Traversable
 --
-class (Functor t, Foldable t) => Traversable' t where
-    traverse' :: Applicative f => (a -> f b) -> t a -> f (t b)
+class (Functor' t, Foldable' t) => Traversable' t where
+    traverse' :: Applicative' f => (a -> f b) -> t a -> f (t b)
 
 instance Traversable' [] where
-    traverse' f [] = pure []
-    traverse' f (h:t) = pure (:) <*> f h <*> traverse' f t
+    traverse' f [] = pure' []
+    traverse' f (h:t) = pure' (:) `apply'` f h `apply'` traverse' f t
+
+-- MEMO: 正直List以外のTraversable'以外の使い道がよくわからない
+instance Traversable' Maybe where
+    traverse' f Nothing = pure' Nothing
+    traverse' f (Just x) = pure' Just `apply'` f x 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Typeclass definitions and implementaions.
 Most of the code is from [GHC.Base](https://hackage.haskell.org/package/base-4.17.0.0/docs/src/GHC.Base.html)
+
+## TODO:
+* data型も独自のもので書き直してみる
